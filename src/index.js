73af7ade424d895734856afea66e139eef9ea7d6
Ivan Nikitin <nikitin.ivan@breezzor.ru>
2021-12-20 20:50:11 +0300

    feat: make Loader

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ import { NavigationComponent } from './components/navigation.component'
 import { CreateComponent } from './components/create.component'
 import { FavoriteComponent } from './components/favorite.component'
 import { PostsComponent } from './components/posts.component'
+import { LoaderComponent } from './components/loader.component'
 
 new HeaderComponent('header')
 
+const loader = new LoaderComponent('loader')
 const navigation = new NavigationComponent('navigation')
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', {loader})
 const create = new CreateComponent('create')
 const favorite = new FavoriteComponent('favorite')
 
